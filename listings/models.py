5badf312c815d19d8447b63664639c695d7a29e9
Lenny Thobejane <Lennythobejane@gmail.com>
2024-09-26 15:22:24 +0200

    added django rest framework

--- listings/models.py
+++ listings/models.py
@@ -1,3 +1,44 @@
 from django.db import models
 
+
 # Create your models here.
+class Band(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    year_formed = models.DateField()
+    genre = models.CharField(max_length=50)
+    photo = models.ImageField()
+    is_active = models.BooleanField(default=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Event(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.CharField(max_length=300)
+    date = models.DateField()
+    time = models.TimeField()
+    location = models.CharField(max_length=100)
+    band = models.ForeignKey(Band, on_delete=models.CASCADE)
+    is_active = models.BooleanField(default=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Listing(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.CharField(max_length=300)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    photo = models.ImageField()
+    is_active = models.BooleanField(default=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
